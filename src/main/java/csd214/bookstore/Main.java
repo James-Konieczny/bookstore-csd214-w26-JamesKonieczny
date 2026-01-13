d795fa2680280580d7f9e1742eafda41afb02419
JamesKonieczny <22037813@saultcollege.ca>
2026-01-13 13:22:01 -0500

    commit 1 for lab-01

--- src/main/java/csd214/bookstore/Main.java
+++ src/main/java/csd214/bookstore/Main.java
@@ -2,7 +2,9 @@ package csd214.bookstore;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bookstore Application Started");
+//        System.out.println("Bookstore Application Started");
+        System.out.println("James Konieczny's Superstore - v1.0");
+
         new App().run();
     }
 }
---
+++ src/main/java/csd214/bookstore/pojos/Stationary.java
@@ -0,0 +1,56 @@
+package csd214.bookstore.pojos;
+
+import java.util.Objects;
+
+public abstract class Stationary extends Product{
+    private String brand;
+
+  // shortcut: alt + insert
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Stationary() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Stationary{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Stationary that)) return false;
+        return Objects.equals(brand, that.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(brand);
+    }
+
+    @Override
+
+    public void initialize() {
+//        super.initialize(); // Critical: let Parent ask for Name/Price
+        System.out.println("Enter Brand:");
+        this.brand = getInput("Generic");
+    }
+
+    @Override
+    public void edit() {
+        // 1. Edit Parent fields (Title, Price, Copies)
+//        super.edit();
+
+        // 2. Edit Self fields
+        System.out.println("Edit Author [" + this.brand + "]:");
+        this.brand = getInput(this.brand);
+    }
+}
