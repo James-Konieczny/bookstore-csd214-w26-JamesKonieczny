a7635dac42f6f2aa3567b40bab320c3ff7a77439
JamesKonieczny <22037813@saultcollege.ca>
2026-01-27 11:51:12 -0500

    bowlingball added to test

---
+++ src/main/java/csd214/bookstore/pojos/BowlingBall.java
@@ -0,0 +1,50 @@
+package csd214.bookstore.pojos;
+
+public class BowlingBall extends BowlingSupply {
+    private int weight;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public BowlingBall(){}
+
+    public BowlingBall(int weight, String color, double price, int copies){
+        super(color, price, copies);
+        this.weight = weight;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        System.out.println("Enter Ball Weight (lbs):");
+        this.weight = getInput(12);
+
+        super.initPriceCopies();
+    }
+
+    @Override
+    public void sellItem() {
+        System.out.println("Selling " + weight + "lb Bowling Ball (Color " + getColor() + ")...");
+        setCopies(getCopies() - 1);
+    }
+
+    @Override
+    public void edit() {
+        super.edit();
+
+        System.out.println("Edit Weight [" + this.weight + "]:");
+        this.weight = getInput(this.weight);
+    }
+
+    @Override
+    public String toString() {
+        return "Bowling Ball{weight='" + weight + "', " + super.toString() + "}";
+    }
+}
+
