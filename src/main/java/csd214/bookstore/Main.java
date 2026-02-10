acfd5ee8a9886d4d7a4b5b8faf65b584898f9f4f
Fred Carella <fred.carella@gmail.com>
2026-02-09 21:06:31 -0500

    commit

---
+++ src/main/java/csd214/bookstore/Main.java
@@ -0,0 +1,8 @@
+package csd214.bookstore;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Freds Carella's Superstore - v1.0");
+        new App().run();
+    }
+}
